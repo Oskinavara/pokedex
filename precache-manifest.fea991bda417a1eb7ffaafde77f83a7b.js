4f72c2a2075c5b48afbc324b22be6b71807748cf
Oskar <oskinavara@gmail.com>
2019-06-11 15:18:28 +0200

    Updates

---
+++ precache-manifest.fea991bda417a1eb7ffaafde77f83a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7f78a900ce940bfc1ce992f547a01b",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "215b3352419894e18948",
+    "url": "/pokedex/static/css/main.00f9ebf6.chunk.css"
+  },
+  {
+    "revision": "7d2056e5229e3433c8b3",
+    "url": "/pokedex/static/js/2.e041075b.chunk.js"
+  },
+  {
+    "revision": "215b3352419894e18948",
+    "url": "/pokedex/static/js/main.7a553fcf.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "9ec29e2ef467f3bbe15bd55e9cd1de32",
+    "url": "/pokedex/static/media/magnifier.9ec29e2e.svg"
+  }
+]);
