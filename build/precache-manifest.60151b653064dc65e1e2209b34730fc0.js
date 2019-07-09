f71e3fe4899a452eb75eac8af0f242cb61ec3236
Oskar <oskinavara@gmail.com>
2019-07-09 14:09:08 +0200

    Deploy

---
+++ build/precache-manifest.60151b653064dc65e1e2209b34730fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bb370f960b4983b3c9fb872e475b89",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "684aeac9cdc646540fda",
+    "url": "/pokedex/static/css/main.a886fe09.chunk.css"
+  },
+  {
+    "revision": "7d2056e5229e3433c8b3",
+    "url": "/pokedex/static/js/2.e041075b.chunk.js"
+  },
+  {
+    "revision": "684aeac9cdc646540fda",
+    "url": "/pokedex/static/js/main.bdf88188.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "9ec29e2ef467f3bbe15bd55e9cd1de32",
+    "url": "/pokedex/static/media/magnifier.9ec29e2e.svg"
+  }
+]);
